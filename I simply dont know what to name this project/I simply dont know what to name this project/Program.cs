ebf1a4be58537ed43d43e6c34c266416615fec72
Mancitiss <vovuongthanhtu@gmail.com>
2021-09-30 01:35:00 +0700

    version 0
    first time created

---
+++ I simply dont know what to name this project/I simply dont know what to name this project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_simply_dont_know_what_to_name_this_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
