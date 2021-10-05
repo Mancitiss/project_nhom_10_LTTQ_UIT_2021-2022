b308c2d10a7fcf91bad0fa03a9ce833e6a60cf26
dai loi <dailoi280702@gmail.com>
2021-10-05 11:16:23 +0700

    form login + sign up

---
+++ A Friend/A Friend/FormLogin.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace A_Friend
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+            labelWarning.Text = "";
+        }
+
+        private void ResetTexts()
+        {
+            textBoxUserName.Texts = "";
+            textBoxPassword.Texts = "";
+            labelWarning.Text = "";
+        }
+
+        private void buttonSignUp_Click(object sender, EventArgs e)
+        {
+            var frm = new FormSignUp();
+            frm.Location = this.Location;
+            frm.StartPosition = FormStartPosition.Manual;
+            frm.FormClosing += delegate { this.Show(); };
+            this.ResetTexts();
+            this.Hide();
+            Thread.Sleep(100);
+            frm.Show();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonLogIn_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void Login()
+        {
+            if (this.EmptyTextBoxes())
+            {
+                if (EmptyTextBoxes())
+                {
+                    labelWarning.Text = "Something is missing?";
+                    return;
+                }
+            }
+            if (!CorrectPassword())
+            {
+                labelWarning.Text = "User name or Password is incorrect";
+                return;
+            }
+            labelWarning.Text = "You have sign up successfully".ToUpper();
+            labelWarning.ForeColor = Color.FromArgb(37, 75, 133);
+            timerClosing.Start();
+        }
+
+        private bool CorrectPassword()
+        {
+            if (textBoxUserName.Texts.Trim() == "admin" && textBoxPassword.Texts.Trim() == "123456")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool EmptyTextBoxes()
+        {
+            if(string.IsNullOrEmpty(textBoxUserName.Texts) || string.IsNullOrEmpty(textBoxPassword.Texts))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void textBoxUserName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if  (e.KeyCode == Keys.Enter) 
+            {
+                buttonLogIn.Focus();
+                Login();
+            }
+        }
+
+        private void textBoxPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonLogIn.Focus();
+                Login();
+            }
+        }
+
+        private void textBoxUserName_Enter(object sender, EventArgs e)
+        {
+            if (labelWarning.Text == "" || labelWarning.Text == "Something is missing?")
+            {
+                return;
+            }
+            this.ResetTexts();
+        }
+
+        private void textBoxPassword_Enter(object sender, EventArgs e)
+        {
+            if (labelWarning.Text == "" || labelWarning.Text == "Something is missing?")
+            {
+                return;
+            }
+            this.ResetTexts();
+        }
+
+        private void timerClosing_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0)
+            {
+                if (this.Opacity > 0.995)
+                {
+                    this.Opacity -= 0.01;
+                }
+                else
+                this.Opacity -= 0.995;
+            }
+            else
+            {
+                timerClosing.Stop();
+                var frm = new FormApp();
+                frm.Location = this.Location;
+                frm.StartPosition = FormStartPosition.Manual;
+                frm.FormClosing += delegate { this.Close(); };
+                this.Hide();
+                frm.Show();
+            }
+        }
+    }
+}
--- A Friend/A Friend/Program.cs
+++ A Friend/A Friend/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace A_Friend
 {
@@ -10,6 +11,9 @@ namespace A_Friend
     {
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormLogin());
         }
     }
 }
