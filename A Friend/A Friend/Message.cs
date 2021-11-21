124bf57ea14009c59acdee95a71e4768b8c8f715
dai loi <dailoi280702@gmail.com>
2021-11-21 18:06:09 +0700

    Add class Message.cs, upgrade ChatItem to show time and create new method to show message with time.

---
+++ A Friend/A Friend/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Friend
+{
+    public class Message
+    {
+        public string text;
+        public string author;
+        public DateTime time;
+
+        // author = "" -> current user
+        public Message(string author, string text, DateTime time)
+        {
+            this.author = author;
+            this.text = text;
+            this.time = time;
+        }
+    }
+}
