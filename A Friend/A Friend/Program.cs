c8fc5269792be810fb2bf35cfa7617cfd6b043ab
Mancitiss <vovuongthanhtu@gmail.com>
2022-01-27 17:15:06 +0700

    Update Program.cs

--- A Friend/A Friend/Program.cs
+++ A Friend/A Friend/Program.cs
@@ -14,7 +14,7 @@ namespace A_Friend
     class Program
     {
         public static FormApplication mainform;
-        internal static int[] thisversion = {2, 6, 1, 1};
+        internal static int[] thisversion = {2, 6, 1, 2};
 
         static void Main(string[] args)
         {
